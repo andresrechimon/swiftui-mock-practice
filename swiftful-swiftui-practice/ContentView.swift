349c31a2017dc63fdf87cf661f2dd366392bf8f6
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-05-26 15:15:44 -0300

    Initial Commit

---
+++ swiftful-swiftui-practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftful-swiftui-practice
+//
+//  Created by Andres Rechimon on 26/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftful-swiftui-practice/swiftful_swiftui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftful_swiftui_practiceApp.swift
+//  swiftful-swiftui-practice
+//
+//  Created by Andres Rechimon on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftful_swiftui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
